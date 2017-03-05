f2cbe0a9417a206d11b2956c8140a56639d9409e
Anton Krinitsyn <Kr1an@hotmail.com>
2017-03-05 12:25:12 +0300

    wp-project/feature: add some phisics

---
+++ warmup-project-lazy-space/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+	private Rigidbody rb;
+	public float Speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * Speed;
+
+	}
+	
+}
